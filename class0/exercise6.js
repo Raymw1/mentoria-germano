553cbf49b86471f969efd81c67036609be11e369
Raymw1 <rayan.rmw@gmail.com>
2021-09-11 10:26:24 -0300

    Fixing exercise 6

--- class0/exercise6.js
+++ class0/exercise6.js
@@ -1,14 +1,18 @@
 function myFunc() {
+  let totalAge = null;
   for (index in arguments) {
-    const argument = arguments[index]
+    let argument = arguments[index]
     if (typeof argument == "string") {
       try {
-        console.log(JSON.parse(argument));
+        argument = JSON.parse(argument);
+        console.log(argument);
+        if (argument.age) totalAge += Number(argument.age);
       } catch (err) {
         console.log("Não é JSON");
       }
     }
   }
+  console.log(`Soma das idades: ${totalAge}!`);
 }
 
 myFunc(15, '{"name": "John"}', "", '{"age": "30"}', '{"age": "45"}')
